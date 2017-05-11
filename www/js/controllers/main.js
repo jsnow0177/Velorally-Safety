a9c907667b27a83499c448273356cd529dbe4dfe
Pavel Balyuk <jsnow0177@gmail.com>
2017-05-11 14:06:02 +0300

    Обновление контроллера

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -3,7 +3,11 @@ Core.defineController('main', function(){
     return {
         main: function(){
             Core.view('main', function(){
-                
+                var $edit_btn = $('#button-edit');
+
+                $edit_btn.on('click', function(){
+                    Core.callController('index', {force: true});
+                });
             });
         }
     };
