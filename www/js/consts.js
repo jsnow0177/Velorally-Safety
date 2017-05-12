8895865c4ffd9e217aee33a8ad732e422f771d0b
Pavel Balyuk <jsnow0177@gmail.com>
2017-05-12 15:19:13 +0300

    VLR_HLPR_UPDATE_CHECK_TIMEOUT constant added

--- www/js/consts.js
+++ www/js/consts.js
@@ -2,3 +2,4 @@ const VLR_HLPR_APP_SECRET = 'Anqhr1sfjxhwHfCJOXef3nlKZQuXMs5ze7xaVrIY';
 const VLR_HLPR_URL = 'http://100.velorally.ua';
 const VLR_HLPR_PING_TIMEOUT = 5000;
 const VLR_HLPR_HELP_REQUEST_TIMEOUT = 1500;
+const VLR_HLPR_UPDATE_CHECK_TIMEOUT = 10000;
